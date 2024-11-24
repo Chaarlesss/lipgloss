1a8b5cb580a6921015343df9d427501c9673e891
Gogume1er <gogume1er@gmail.com>
2024-11-24 16:15:20 +0100

    remove unused file

--- lib/gamut.ml
+++
@@ -1,6 +0,0 @@
-(* L120 *)
-let blends c1 c2 count =
-  let dl = 1. /. Float.of_int (count + 1) in
-  Array.init count (fun i ->
-      Colorful.color_blend_lab c1 c2 (dl *. Float.of_int (i + 1))
-      |> Colorful.color_clamped )
